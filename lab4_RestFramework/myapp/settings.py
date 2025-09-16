5ead7cb9a1497203f3a3fadabdf02975a9d81924
Lezt Waper <leztwaper@gmail.com>
2025-09-16 22:33:24 +0700

    Finish the tutorial

--- lab4_RestFramework/myapp/settings.py
+++ lab4_RestFramework/myapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'snippets',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Default primary key field type
+# Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
